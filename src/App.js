5bf8bd02a0eac2076df7ec66efb226b443e986d3
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-21 12:08:29 -0600

    changed state

--- src/App.js
+++ src/App.js
@@ -12,11 +12,6 @@ const taskData = [
     task: 'Bake Cookies',
     id: 1528817084358,
     completed: false
-  },
-  {
-    task: 'Mow Grass',
-    id: Date.now(),
-    completed: false
   }
 ];
 
